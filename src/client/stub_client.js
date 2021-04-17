6e668464cce53fdaa56a388e513498b60ad88f76
PopovaMaria <49453011+PopovaMaria@users.noreply.github.com>
2021-04-17 11:27:05 +0300

    now u can add tasks (#6)
    * now u can add tasks
    
    * компоненты карточек создаются в render
    
    * componentDidMount removed
    
    * now adding all cards in the stage state directly
    
    * deleted \n

--- src/client/stub_client.js
+++ src/client/stub_client.js
@@ -1,10 +1,20 @@
 import board_data from '../example_data/board.json'
+import { v4 as uuid4 } from 'uuid';
 
 
 class StubClient {
 
     getBoard() {
         return board_data;
+    }
+
+    addNewCard(stage) {
+        const newId = uuid4();
+        return {
+            "id": newId,
+            "stage": stage,
+            "title": "Make release v0.2.0"
+        };
     }
 
 }
--- src/components/stage.js
+++ src/components/stage.js
@@ -1,20 +1,43 @@
 import Card from "./card";
 import React, {Component} from "react";
+import Button from '@material-ui/core/Button';
 
 
 class Stage extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: this.props.cards
+    };
+  }
+
+  createCard = (stage) => {
+    let newCard = this.props.client.addNewCard(stage)
+    let newCards = this.state.cards
+    newCards.push(newCard)
+    this.setState({cards: newCards})
+}
+
     render() {
-        let cards = []
 
-        for (const card of this.props.cards) {
-            let component = <Card id={card.id} title={card.title}/>
-            cards.push(component)
-        }
+      let cards = []
 
+      for (const card of this.state.cards) {
+        let component = <Card id={card.id} title={card.title}/>
+        cards.push(component)
+      }
         return (
             <div style={style}>
+            <div>
                 {cards}
+            </div>
+              <Button
+                onClick={
+                  () => this.createCard(this.props.title)
+                }
+              >Add another card
+              </Button>
             </div>
         );
     }
@@ -28,3 +51,4 @@ const style = {
 }
 
 export default Stage;
+
